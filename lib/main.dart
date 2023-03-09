6e241fe8ea4276e8b9b92bb3f8799cf43be8f32d
Louis Alary <louis.alary34@gmail.com>
2023-03-09 16:57:55 +0100

    Feature : Added package providers and dark mode integration in settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intervaltimer/providers/dark_theme.dart';
 import 'package:intervaltimer/screens/edit_timer_screen.dart';
 import 'package:intervaltimer/screens/home_screen.dart';
 import 'package:intervaltimer/screens/new_timer_screen.dart';
 import 'package:intervaltimer/screens/play_timer_screen.dart';
 import 'package:intervaltimer/screens/settings_screen.dart';
+import 'package:provider/provider.dart';
 import 'dummy_data.dart';
 import 'model/timer.dart';
 
@@ -11,14 +13,27 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => DarkTheme(),
+      child: const MyHomePage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
 
   List<Timer> _userTimers = DUMMY_TIMER;
 
@@ -30,20 +45,23 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'OpenSans'
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => HomeScreen(userTimers: _userTimers),
-        NewTimerScreen.routeName: (ctx) => NewTimerScreen(
-            addNewTimer: _addNewTimer),
-        PlayTimerScreen.routeName: (ctx) => const PlayTimerScreen(),
-        EditTimerScreen.routeName: (ctx) => const EditTimerScreen(),
-        SettingsScreen.routeName: (ctx) => const SettingsScreen(),
-      },
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: 'OpenSans',
+        ),
+        darkTheme: ThemeData.dark(), // standard dark theme
+        themeMode: Provider.of<DarkTheme>(context).isDarkMode ?
+                        ThemeMode.dark : ThemeMode.light, // device controls theme
+        initialRoute: '/',
+        routes: {
+          '/': (ctx) => HomeScreen(userTimers: _userTimers),
+          NewTimerScreen.routeName: (ctx) => NewTimerScreen(
+              addNewTimer: _addNewTimer),
+          PlayTimerScreen.routeName: (ctx) => const PlayTimerScreen(),
+          EditTimerScreen.routeName: (ctx) => const EditTimerScreen(),
+          SettingsScreen.routeName: (ctx) => const SettingsScreen(),
+        },
+      );
   }
 }
